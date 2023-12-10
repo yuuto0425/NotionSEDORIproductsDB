77f7684e861223ec08e1961b527a23d0b49b7de6
yuuto0425 <dog905469@gmail.com>
2023-12-10 20:17:16 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -26,16 +26,21 @@ function WebForm() {
 
   const getStockPriceValue = () => {
     const StockPriceValue = queryParams.get("stock-price");
-    return StockPriceValue ? decodeURIComponent(StockPriceValue).replace(/"/g, "") : "";
+    return StockPriceValue
+      ? decodeURIComponent(StockPriceValue).replace(/"/g, "")
+      : "";
   };
 
   const textAreaRef = useRef(null);
+
+  const [isCopyButtonClicked, setIsCopyButtonClicked] = useState(false);
 
   const handleCopyToClipboard = async () => {
     try {
       const textToCopy = textAreaRef.current.value;
       await copy(textToCopy);
       alert("テキストがクリップボードにコピーされました");
+      setIsCopyButtonClicked(true); // コピーが成功したら状態を更新
     } catch (error) {
       alert("クリップボードへのコピーに失敗しました");
     }
@@ -49,16 +54,43 @@ function WebForm() {
 
   const [feedText, setFeedText] = useState("");
 
+  const [selectedPurpose, setSelectedPurpose] = useState([]);
+  
+  const [checkedItems, setCheckedItems] = useState({
+    negotiationEvidence: false,
+    trimming: false,
+    priceIncreaseNotification: false,
+    trendTracking: false,
+    procurementDecision: false,
+    collection: false,
+  });
+
+  // const handleCheckboxChange = (itemName) => {
+  //   setCheckedItems((prevItems) => ({
+  //     ...prevItems,
+  //     [itemName]: !prevItems[itemName],
+  //   }));
+  // };
+
   // let response;
   const handleFormSubmit = async () => {
     try {
+      const selectedPurposeData = purposeOptions
+        .filter((purpose) => selectedPurpose.includes(purpose))
+        .map((selectedPurpose) => ({
+          [selectedPurpose]: checkedItems[selectedPurpose],
+        }));
+
+
+        // `https://notion-products-db-server-app-bee944b6684d.herokuapp.com/createNotionPage` 
       const response = await axios.post(
-        `https://notion-products-db-server-app-bee944b6684d.herokuapp.com/createNotionPage`||`http://localhost:3001/createNotionPage`,
+        `http://localhost:3001/createNotionPage`,
         {
           product,
           asin,
           StockPriceValue,
           feedback,
+          selectedPurpose,
         }
       );
       // const {properties} = [response.data];
@@ -80,10 +112,46 @@ function WebForm() {
 
   // const FeedText = response.data.data.properties
 
+  const purposeOptions = [
+    "値下げ交渉エビデンス",
+    "刈り取り",
+    "値上げ通知",
+    "動向追い",
+    "仕入れ判断",
+    "回収",
+  ];
+
+  const handlePurposeChange = (e) => {
+    const value = e.target.value;
+    if (selectedPurpose.includes(value)) {
+      setSelectedPurpose(
+        selectedPurpose.filter((purpose) => purpose !== value)
+      );
+    } else {
+      setSelectedPurpose([...selectedPurpose, value]);
+    }
+  };
+  console.log(selectedPurpose,setSelectedPurpose)
+
   return (
     <div className="form-container">
       <h2>Web Form</h2>
       <form>
+        <div className="form-input">
+          <label>仕入れ値</label>
+          <input
+            type="text"
+            value={StockPriceValue}
+            onChange={(e) => setStockPriceValue(e.target.value)}
+          />
+        </div>
+        <div className="form-input">
+          <label>感想</label>
+          <textarea
+            value={feedback}
+            onChange={(e) => setFeedback(e.target.value)}
+          />
+        </div>
         <div className="form-input">
           <label>商品名</label>
           <input
@@ -101,36 +169,33 @@ function WebForm() {
           />
         </div>
         <div className="form-input">
-          <label>仕入れ値</label>
-          <input
-            type="text"
-            value={StockPriceValue}
-            onChange={(e) => setStockPriceValue(e.target.value)}
-          />
-        </div>
-
-        <div className="form-input">
-          <label>感想</label>
-          <textarea
-            value={feedback}
-            onChange={(e) => setFeedback(e.target.value)}
-          />
+          <label>目的別</label>
+          <div className="checkbox-options">
+            {purposeOptions.map((purpose) => (
+              <label key={purpose} className="checkbox-label">
+                <input
+                  type="checkbox"
+                  value={purpose}
+                  checked={selectedPurpose.includes(purpose)}
+                  onChange={handlePurposeChange}
+                />
+                <p>{purpose}</p>
+              </label>
+            ))}
+          </div>
         </div>
       </form>
-      <div className="form-input">
+      {/* <div className="form-input">
         <Link to={`/form?product=${product}&asin=${asin}`}>保存</Link>
-      </div>
+      </div> */}
       <div>
         <button type="button" onClick={handleFormSubmit}>
           送信
         </button>
       </div>
       <div>
-        <textarea
-          ref={textAreaRef}
-          defaultValue= {feedText}
-        />
-        <button onClick={handleCopyToClipboard}>クリップボードにコピー</button>
+        <textarea ref={textAreaRef} defaultValue={feedText} />
+        <button className={`onCopy ${isCopyButtonClicked ? "copiedButton" : ""}`} onClick={handleCopyToClipboard}>クリップボードにコピー</button>
       </div>
     </div>
   );
